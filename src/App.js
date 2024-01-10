c63ce651d0198be8a453f5f78f5acacf2f31c57e
Teja <dharamtej31@gmail.com>
2024-01-10 17:01:18 +0530

    made some changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+       Teja Adabala --- Created a React app thanks for watching
         </p>
         <a
           className="App-link"
